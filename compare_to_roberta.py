6c1e8564c69795e0cdfb773d68dde4fbd0d85573
icbaptista <ines.castro.batista@gmail.com>
2024-05-08 18:55:14 +0100

    updating compare to roberta script

--- compare_to_roberta.py
+++ compare_to_roberta.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from datasets import Dataset, load_metric
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import numpy as np
 import torch
@@ -9,20 +8,11 @@ import matplotlib.pyplot as plt
 tokenizer = AutoTokenizer.from_pretrained("Hello-SimpleAI/chatgpt-detector-roberta")
 model = AutoModelForSequenceClassification.from_pretrained("Hello-SimpleAI/chatgpt-detector-roberta")
 
-def load_and_tokenize_dataset(dataset_path):
-    test_data = pd.read_csv(dataset_path)
-    test_dataset = Dataset.from_pandas(test_data)
-    def tokenize_function(examples):
-        return tokenizer(examples["text"], padding="max_length", truncation=True)
-    tokenized_dataset = test_dataset.map(tokenize_function, batched=True)
-    return tokenized_dataset
-
 def is_ai_generated(file_path, model, tokenizer):
-    # Read the content of the file
-    with open(file_path, 'r') as file:
-        text = file.read().strip()  # Read the file content and strip any leading/trailing whitespace
-
-    inputs = tokenizer(text, return_tensors="pt", max_length=512, truncation=True)
+    reader = pd.read_csv(file_path, chunksize=1)
+    for chunk in reader: 
+        row = chunk.iloc[0, 0]
+        inputs = tokenizer(row, return_tensors="pt", max_length=512, truncation=True)
 
     with torch.no_grad():
         outputs = model(**inputs)
@@ -42,26 +32,10 @@ def plot_confusion_matrix(TP, TN, FP, FN, labels=[1, 0]):
     ax.tick_params(axis='both', which='major', labelsize=10, labelbottom = False, bottom=False, top = True, labeltop=True)
     plt.show()
 
-def compute_metrics(predictions, labels):
-    accuracy = load_metric('accuracy')
-    f1 = load_metric('f1')
-    recall = load_metric('recall')
-    confusion_matrix = load_metric('confusion_matrix')
-
-    accuracy_result = accuracy.compute(predictions=predictions, references=labels)
-    f1_result = f1.compute(predictions=predictions, references=labels, average='weighted')
-    recall_result = recall.compute(predictions=predictions, references=labels, average='weighted')
-    confusion_matrix_result = confusion_matrix.compute(predictions=predictions, references=labels)
-
-    print(f"Accuracy: {accuracy_result['accuracy']}")
-    print(f"F1 Score: {f1_result['f1']}")
-    print(f"Recall: {recall_result['recall']}")
-    print(confusion_matrix_result)
 
 def main():
     dataset_path = "archive/final_test.csv"
-    tokenized_dataset = load_and_tokenize_dataset(dataset_path)
-    
+     
     # Example usage of is_ai_generated function
     #text = "This is a sample text."
     ai_generated = is_ai_generated(dataset_path, model, tokenizer)
@@ -70,7 +44,6 @@ def main():
     # To compute metrics, you would need to iterate over your dataset and collect predictions
     # This example shows how to use the function for a single text input
     #plot_confusion_matrix(100, 100, 100, 100)
-    # compute_metrics(predictions, labels)
 
 if __name__ == "__main__":
     main()
